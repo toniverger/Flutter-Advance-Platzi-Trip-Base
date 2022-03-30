7e33a762e5139b435f9c9c1ba6c5e3880b341c1f
Toni Verger <toniverger@msn.com>
2022-03-30 16:26:14 -0300

    fix in structure to complain BLoC

--- lib/User/ui/widgets/profile_place.dart
+++ lib/User/ui/widgets/profile_place.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:platzi_trips_app/place.dart';
+import 'package:platzi_trips_app/Place/model/place.dart';
 import 'profile_place_info.dart';
-import 'profile_place.dart';
 
 class ProfilePlace extends StatelessWidget {
 
